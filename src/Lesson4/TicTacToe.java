d4d51b29f0e16ea26dd4d9d0ce6ac973f4e9f467
Anna <baklan.anna@gmail.com>
2021-03-01 18:19:15 +0300

    Практическое занятие №4.

--- src/Lesson4/TicTacToe.java
+++ src/Lesson4/TicTacToe.java
@@ -7,15 +7,16 @@ public class TicTacToe {
 
     private static Scanner scan = new Scanner(System.in);
     private static Random random = new Random();
-    private static int fieldSize = 5;
-    private static int dotsToWin = 5;
-    private static char[][] playField = new char[fieldSize][fieldSize];
-    private static char emptyDot = '*';
-    private static char humanDot = 'X';
-    private static char computerDot = 'O';
-    private static int X;
-    private static int Y;
-    private enum Mode {HUMAN, COMPUTER};
+    private static final int fieldSize = 5;
+    private static final int dotsToWin = 5;
+    private static final char[][] playField = new char[fieldSize][fieldSize];
+    private static final char emptyDot = '*';
+    private static final char humanDot = 'X';
+    private static final char computerDot = 'O';
+
+    private enum Mode {HUMAN, COMPUTER}
+
+    ;
     private static int MIN_DOTS_TO_CHECK = 2;
 
     public static void main(String[] args) {
@@ -23,7 +24,7 @@ public class TicTacToe {
         initMap();
         printMap();
 
-        while(true){
+        while (true) {
             humanStep();
             printMap();
             if (checkWin(humanDot)) {
@@ -49,18 +50,16 @@ public class TicTacToe {
     }
 
     private static void computerStep() {
-        if (findBestStep()) {
-            return;
-        } else  {
-                do {
-                    X = random.nextInt(fieldSize);
-                    Y = random.nextInt(fieldSize);
-                } while (!checkXY (X, Y, Mode.COMPUTER));
-            playField[X][Y] = computerDot;
-            }
+        if (!findBestStep()) {
+            int x;
+            int y;
+            do {
+                x = random.nextInt(fieldSize);
+                y = random.nextInt(fieldSize);
+            } while (!checkXY(x, y, Mode.COMPUTER));
+            playField[x][y] = computerDot;
+        }
     }
-
-
 
 
     private static boolean findBestStep() {
@@ -69,18 +68,15 @@ public class TicTacToe {
             return false;
         } else {
             for (int i = dotsToWin; i > 1; i--) {
-                if (checkAllLinesForNextStep(i, Mode.COMPUTER)) {
-                    return true;
-                } else if (checkAllLinesForNextStep(i, Mode.HUMAN)) {
+                if (checkAllLinesForNextStep(i, Mode.COMPUTER) || checkAllLinesForNextStep(i, Mode.HUMAN)) {
                     return true;
                 }
-
             }
             return false;
         }
     }
 
-    private static boolean checkAllLinesForNextStep (int stepsToCheck, Mode mode) {
+    private static boolean checkAllLinesForNextStep(int stepsToCheck, Mode mode) {
 
         for (int j = 0; j < fieldSize; j++) {
             for (int k = 0; k < fieldSize; k++) {
@@ -95,54 +91,53 @@ public class TicTacToe {
         return false;
     }
 
-    private static boolean checkLineForNextStep (char dotSymbolToSet, Mode mode, int stepsToCheck, int x, int y, int dx, int dy) {
-        if ((x + dotsToWin*dx) > fieldSize || (y + dotsToWin*dy) > fieldSize || (x + (dotsToWin-1)*dx) < 0 || (y + (dotsToWin-1)*dy) < 0 ) {
+    private static boolean checkLineForNextStep(char dotSymbolToSet, Mode mode, int stepsToCheck, int x, int y, int dx, int dy) {
+        if ((x + dotsToWin * dx) > fieldSize || (y + dotsToWin * dy) > fieldSize || (x + (dotsToWin - 1) * dx) < 0 || (y + (dotsToWin - 1) * dy) < 0) {
             return false;
         }
         int emptyFields = 0;
-        int [] x1 = new int [dotsToWin];
-        int [] y1 = new int [dotsToWin];
+        int[] x1 = new int[dotsToWin];
+        int[] y1 = new int[dotsToWin];
         for (int i = 0; i < dotsToWin; i++) {
             if (mode == Mode.COMPUTER && playField[x][y] == humanDot) {
                 return false;
-            } else if (mode == Mode.HUMAN && playField[x][y] == computerDot){
+            } else if (mode == Mode.HUMAN && playField[x][y] == computerDot) {
                 return false;
             } else if (playField[x][y] == emptyDot && emptyFields <= dotsToWin - stepsToCheck) {
-                emptyFields += 1;
-                x1[emptyFields-1] = x;
-                y1[emptyFields-1] = y;
+                emptyFields ++;
+                x1[emptyFields - 1] = x;
+                y1[emptyFields - 1] = y;
             } else if (playField[x][y] == emptyDot && emptyFields > dotsToWin - stepsToCheck) {
                 return false;
             }
-            x+=dx;
-            y+=dy;
+            x += dx;
+            y += dy;
         }
-        if (emptyFields > 0)  {
+        if (emptyFields > 0) {
             int p = emptyFields == 1 ? 0 : random.nextInt(emptyFields - 1);
-            playField [x1[p]][y1[p]] = dotSymbolToSet;
+            playField[x1[p]][y1[p]] = dotSymbolToSet;
             return true;
         }
         return false;
     }
 
 
-
-    private static boolean checkWin(char DotSymbol) {
+    private static boolean checkWin(char dotSymbol) {
         for (int i = 0; i < fieldSize; i++) {
             for (int j = 0; j < fieldSize; j++) {
-                if (playField[i][j] == DotSymbol && (checkLine(DotSymbol, i, j, (byte)1, (byte)0) ||
-                        checkLine(DotSymbol, i, j, (byte)0, (byte)1)||
-                        checkLine(DotSymbol, i, j, (byte)1, (byte)1)||
-                        checkLine(DotSymbol, i, j, (byte)-1, (byte)1))) {
-                        return true;
-                    }
+                if (playField[i][j] == dotSymbol && (checkLine(dotSymbol, i, j, (byte) 1, (byte) 0) ||
+                        checkLine(dotSymbol, i, j, (byte) 0, (byte) 1) ||
+                        checkLine(dotSymbol, i, j, (byte) 1, (byte) 1) ||
+                        checkLine(dotSymbol, i, j, (byte) -1, (byte) 1))) {
+                    return true;
                 }
             }
+        }
         return false;
     }
 
     private static boolean checkLine(char dotSymbol, int x, int y, byte dx, byte dy) {
-        if ((x + dotsToWin*dx) > fieldSize || (y + dotsToWin*dy) > fieldSize || (x + (dotsToWin-1)*dx) < 0 || (y + (dotsToWin-1)*dy) < 0 ) {
+        if ((x + dotsToWin * dx) > fieldSize || (y + dotsToWin * dy) > fieldSize || (x + (dotsToWin - 1) * dx) < 0 || (y + (dotsToWin - 1) * dy) < 0) {
             return false;
         }
         for (int i = 0; i < dotsToWin; i++) {
@@ -170,23 +165,25 @@ public class TicTacToe {
 
     private static void humanStep() {
         System.out.println("Введите координаты в формате X Y:");
+        int x;
+        int y;
         do {
-            X = scan.nextInt()-1;
-            Y = scan.nextInt()-1;
-        } while (!checkXY (X, Y, Mode.HUMAN));
-        playField[X][Y] = humanDot;
+            x = scan.nextInt() - 1;
+            y = scan.nextInt() - 1;
+        } while (!checkXY(x, y, Mode.HUMAN));
+        playField[x][y] = humanDot;
     }
 
-    private static boolean checkXY(int X, int Y, Mode mode) {
-        if (X < 0 || X >= fieldSize || Y < 0 || Y >= fieldSize) {
+    private static boolean checkXY(int x, int y, Mode mode) {
+        if (x < 0 || x >= fieldSize || y < 0 || y >= fieldSize) {
             System.out.println("Каждая координата должна быть в диапазоне от 1 до " + fieldSize);
             return false;
-        } else if (playField[X][Y] != emptyDot) {
+        } else if (playField[x][y] != emptyDot) {
             if (mode == Mode.HUMAN) {
                 System.out.println("Ячейка уже занята. Пожалуйста, выберите другую");
             }
             return false;
-        }   else {
+        } else {
             return true;
         }
     }
@@ -198,7 +195,7 @@ public class TicTacToe {
         }
         System.out.println();
         for (int i = 0; i < fieldSize; i++) {
-            System.out.print((i+1) + " ");
+            System.out.print((i + 1) + " ");
             for (int j = 0; j < fieldSize; j++) {
                 System.out.print(playField[i][j] + " ");
             }
