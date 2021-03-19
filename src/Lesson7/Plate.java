9ad0d11eaf1b01be1a88f04895b34b6d3077ba63
Anna <baklan.anna@gmail.com>
2021-03-19 10:55:41 +0300

    Практическое занятие №7.

---
+++ src/Lesson7/Plate.java
@@ -0,0 +1,38 @@
+package Lesson7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println("Food amount: " + food);
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood (int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int amount) {
+        if (amount > food) {
+            return false;
+        } else if (amount == 0) {
+            return false;
+        } else if (amount < 0) {
+            return false;
+        }
+        food -= amount;
+        return true;
+    }
+
+    public void addFood (int food) {
+        this.food += food;
+    }
+
+}
